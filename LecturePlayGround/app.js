7b2660a22d56144a546884bfd98a3cad4af6396d
rdfzZFWANG <aldrich-wang@outlook.com>
2022-01-24 12:52:41 -0600

    Module 8
    For Module 3 assignment of the angularjs course.

--- LecturePlayGround/app.js
+++ LecturePlayGround/app.js
@@ -1,34 +1,19 @@
 (function () {
     "use strict";
-    var x = 10;
+
     angular.module("App", [])
-    .controller("MyFirstController", MyFirstController);
+    .controller("Controller", Controller);
     
-    function MyFirstController($scope) {
-        $scope.name = "Aldrich";
-        $scope.Hello = function () {
-            return "Hello World again";
-        }
-        $scope.change = function () {
-            var string = $scope.name;
-            var newString = "";
-            if (string.length > 0) {
-                for (var i = 0; i < string.length; i++) {
-                    newString += string.charCodeAt(i);
-                }
-            }
-            return newString;
-        }
-        $scope.roll = function () {
-            var string = $scope.name;
-            var newString = "";
-            if (string.length > 0) {
-                for (var i = 1; i < string.length; i++) {
-                    newString += string.charAt(i);
-                }
-                newString += string.charAt(0);
-            }
-            $scope.name = newString;
+    Controller.$inject = ["$scope"]
+    function Controller($scope) {
+        var ctrl = this;
+        ctrl.value = 1;
+        ctrl.text = '';
+        ctrl.name = 'Name';
+        ctrl.select = null;
+        $scope.postsList = ['a','b','c'];
+        ctrl.check = function () {
+            ctrl.text = ctrl.select;
         }
     }
 })();
---
+++ module8-solution/app.js
@@ -0,0 +1,59 @@
+(function () {
+    "use strict";
+    angular.module('NarrowItDownApp', [])
+    .service('MenuSearchService', menuSearchService)
+    .controller("NarrowItDownController", narrowItDownController)
+    .directive('foundItems', foundItems);
+
+    menuSearchService.$inject = ['$http']
+    function menuSearchService ($http) {
+        const service = this;
+        
+        service.getMatchedMenuItems = function (searchItem) {
+            return $http({
+                method: "GET",
+                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+            }).then(function (res) {
+                const menuItems = res.data.menu_items;
+                var toRet = [];
+                for (let i = 0; i < menuItems.length; i++) {
+                    if (menuItems[i].description.indexOf(searchItem) !== -1) {
+                        toRet.push(menuItems[i]);
+                        console.log(menuItems[i]);
+                    };
+                };
+                return toRet
+            });
+        };
+    };
+
+    narrowItDownController.$inject = ["MenuSearchService"]
+    function narrowItDownController(service) {
+        const ctrl = this;
+        ctrl.found = null;
+        ctrl.getMatchedMenuItems = function (searchItem) {
+            service.getMatchedMenuItems(searchItem).then(function (res) {
+                console.log(res);
+                ctrl.found = res;
+            });
+        };
+        ctrl.removeItem = function (index) {
+            console.log('removing');
+            ctrl.found.splice(index, 1);
+        };
+    };
+
+    function foundItems() {
+        var ddo = {
+            restrict: 'E',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            },
+
+            templateUrl: 'loader/itemsloaderindicator.template.html'
+        };
+
+        return ddo;
+    };
+})();
